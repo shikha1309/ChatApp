37af13fbd23c962fa139da6a012386b03d44a8da
SHIKHA <shikhayadav1309@gmail.com>
2023-02-18 15:21:11 +0530

    xml for tabLayout

--- app/src/main/java/com/shikha/chatapp/MainActivity.kt
+++ app/src/main/java/com/shikha/chatapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.shikha.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
-class MainActivity : AppCompatActivity() {
+class  MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbarMain)
+        setUpToolbar()
+
+    }
+
+    private fun setUpToolbar() {
+       setSupportActionBar(toolbar)
+        supportActionBar?.title = "Toolbar Title"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
